ccd85c2d9055947e3337816a554a17a9b1973b31
Wilfred Hughes <wilfred@potatolondon.com>
2011-04-18 16:02:22 +0100

    use the modified time of the minified files to decide whether it needs minifying -- stops needless work

--- mashed_potato.py
+++ mashed_potato.py
@@ -34,6 +34,41 @@ def is_minifiable(file_name):
 
     return True
 
+def get_minified_name(file_path):
+    """Convert a file name into its minified version. We don't do a
+    simple .replace() to avoid corner cases, as shown in the example.
+
+    >>> get_minified_name("/a/b/.js/c/foo.js)
+    "/a/b/.js/c/foo.min.js"
+
+    """
+    if file_path.endswith('.js'):
+        minified_path = file_path[:-3] + '.min.js'
+    elif file_name.endswith('.css'):
+        minified_path = file_path[:-4] + '.min.css'
+
+    return minified_path
+
+
+def needs_minifying(file_path):
+    """Returns false only if a file already has a minified file, and
+    the minified file is newer than the source.
+
+    """
+    source_edited_time = os.path.getmtime(file_path)
+
+    last_minified_time = None
+    minifed_file_path = get_minified_name(file_path)
+
+    if os.path.exists(minifed_file_path):
+        last_minified_time = os.path.getmtime(minifed_file_path)
+
+    if last_minified_time and last_minified_time > source_edited_time:
+        return True
+
+    return False
+
+
 def is_ignored(file_path, ignore_strings):
     # exclude what we've been asked to ignore
     for ignore_string in ignore_strings:
@@ -69,8 +104,6 @@ if __name__ == '__main__':
     else:
         print "Monitoring JavaScript and CSS files for changes.\nPress Ctrl-C to quit or Ctrl-Z to stop.\n"
 
-    minified_times = {}
-
     while True:
         time.sleep(1)
 
@@ -80,11 +113,8 @@ if __name__ == '__main__':
                     file_path = os.path.join(path, file_name)
 
                     if is_minifiable(file_name) and not is_ignored(file_path, ignore_strings):
-                        modified_time = os.path.getmtime(file_path)
-                        minified_time = minified_times.get(file_path, None)
 
-                        if not minified_time or modified_time > minified_time:
-                            minified_times[file_path] = modified_time
+                        if needs_minifying(file_path):
                             minify(file_path)
 
                             # inform the user:
